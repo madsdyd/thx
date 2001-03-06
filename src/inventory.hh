339e5d128ae6395f3a0d024f87bda65326275a5c
Mads Bondo Dydensborg <mads@dydensborg.dk>
2001-03-06 22:16:29 +0000

    Added the GPL stuff (COPYING file, header in each file) Updated the README a bit Minor updates to other files.

--- src/inventory.hh
+++ src/inventory.hh
@@ -1,3 +1,24 @@
+/*
+    Tank Hill eXtreme
+    Copyright (C) 2000-2001 Mads Bondo Dydensborg <mads@dydensborg.dk> and 
+    Skjalm Arrøe <skjalm@arroe.dk>
+    Copyright (C) 2001 contributers of the THX project
+    Please see the file "AUTHORS" for a list of contributers
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+ 
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+ 
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 #ifndef __INVENTORY_HH_
 #define __INVENTORY_HH_
 /* This is an inventory that each player can have. It is also used to

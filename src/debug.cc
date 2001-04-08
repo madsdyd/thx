02501a7f369948772103bc0fbb026a8d0aa90cd3
Mads Bondo Dydensborg <mads@dydensborg.dk>
2001-04-08 18:04:35 +0000

    - Added debug file   - Added more -W options to the compile

---
+++ src/debug.cc
@@ -0,0 +1,36 @@
+/*
+    Tank Hill eXtreme
+    Copyright (C) 2000-2001 Mads Bondo Dydensborg <mads@dydensborg.dk> and 
+    Skjalm Arrøe <skjalm@arroe.dk>
+    Copyright (C) 2001 contributers of the THX project
+    Please see the file "AUTHORS" for a list of contributers
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+ 
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+ 
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+#include "debug.hh"
+#ifdef DEBUG
+#include <iostream>
+
+/* If this works well, I want to extend this assert to do some
+   interaction with the user */
+bool CustomAssert(bool expr, char * description, int linenum, char * filename) {
+  if (!expr) {
+    cerr << "CustomAssert failed at " << filename << ":" << linenum << " \""
+	 << description << "\"" << endl;
+    return true;
+  } 
+  return false;
+}
+#endif
